d07aa9bd568cfb6d99a5ea6951b24ee01eb5df92
Enri <ibenrique2510@gmail.com>
2021-03-12 17:14:33 +0700

    event handling

--- app.js
+++ app.js
@@ -10,12 +10,30 @@ var app = new Vue({
         variants: [
             {
                 variantId: 0001,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
             },
             {
                 variantId: 0002,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: './assets/vmSocks-blue-onWhite.jpg',
             }
         ],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            }
+        },
+
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
