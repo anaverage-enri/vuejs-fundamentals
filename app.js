811c5070d23729b0256e86e63cb30b43ddee50dd
Enri <ibenrique2510@gmail.com>
2021-03-12 16:43:40 +0700

    conditional rendering

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
+        inventory: 8,
         image: './assets/vmSocks-green-onWhite.jpg',
         description: 'A pair of warm, fuzzy socks',
     }
