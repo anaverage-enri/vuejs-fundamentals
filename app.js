ad0ad66f9a3904b93a18c236668441d2aff3297f
Enri <ibenrique2510@gmail.com>
2021-03-12 16:56:43 +0700

    list rendering

--- app.js
+++ app.js
@@ -5,5 +5,17 @@ var app = new Vue({
         inventory: 8,
         image: './assets/vmSocks-green-onWhite.jpg',
         description: 'A pair of warm, fuzzy socks',
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        variants: [
+            {
+                variantId: 0001,
+                variantColor: "green"
+            },
+            {
+                variantId: 0002,
+                variantColor: "blue"
+            }
+        ],
     }
 })
